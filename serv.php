7214b42d3fa8532585af2dc557ea0f8df4bfa423
Catman <381332123@qq.com>
2018-03-13 17:21:31 +0800

    pages
    pages

--- serv.php
+++ serv.php
@@ -1,3 +1,5 @@
 <?php
-	echo "true"
+	//echo "true"
+	header("Content-Type: application/json; charset=utf-8"); 
+    echo ('[{"age":24,"sex":"boy","name":"周志学"},{"age":26,"sex":"boy","name":"李宝辉"}]');
 ?>
